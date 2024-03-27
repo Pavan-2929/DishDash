f552d985f96067310c82b8e8a2e1889839bcfde5
Pavan-2929 <kapdiyapavan3218@gmail.com>
2024-03-27 23:55:11 +0530

    Added average rating functionality in the Restaurant Card

--- client/src/components/cards/RestaurantCard.jsx
+++ client/src/components/cards/RestaurantCard.jsx
@@ -1,8 +1,38 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
 import { FiClock, FiDollarSign, FiMapPin } from "react-icons/fi";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
+import axios from "axios";
 
-const RestaurantCard = ({restaurant, path }) => {
+const RestaurantCard = ({ restaurant, path }) => {
+  const [reviews, setReviews] = useState([]);
+
+  const fetchReviews = async (e) => {
+    try {
+      const response = await axios.get(
+        `http://localhost:3000/api/review/get/restaurant/${restaurant._id}`
+      );
+
+      setReviews(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchReviews();
+  }, []);
+
+  const countAverageReviews = () => {
+    if (reviews.length === 0) return 0;
+
+    let total = 0;
+    reviews.map((review) => {
+      total += review.rating;
+    });
+
+    return total / reviews.length;
+  };
+
   return (
     <div
       key={restaurant._id}
@@ -27,9 +57,12 @@ const RestaurantCard = ({restaurant, path }) => {
           <FiDollarSign className="mr-1" />
           <p>Delivery Price: ${restaurant.deliveryPrice}</p>
         </div>
-        <div className="flex items-center text-gray-600">
+        <div className="flex items-center mb-2 text-gray-600">
           <FiClock className="mr-1" />
           <p>Delivery Time: {restaurant.estimatedDeliveryTime} minutes</p>
+        </div>
+        <div className="flex items-center text-gray-600">
+          <p>Average Rating: {countAverageReviews().toFixed(1)} / 5 ★</p>
         </div>
         <div className="mt-6">
           <Link
@@ -42,6 +75,6 @@ const RestaurantCard = ({restaurant, path }) => {
       </div>
     </div>
   );
-}
+};
 
-export default RestaurantCard
+export default RestaurantCard;
