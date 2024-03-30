7dcdce38407a36d9ee5179bb244c81e4d42aef98
Pavan-2929 <kapdiyapavan3218@gmail.com>
2024-03-31 00:22:25 +0530

    fix loading issue in the restaurant page

--- client/src/components/menu/RestaurantMenu.jsx
+++ client/src/components/menu/RestaurantMenu.jsx
@@ -20,6 +20,7 @@ const RestaurantMenu = () => {
         `https://dishdash-server.onrender.com/api/restaurant/get/${params.id}`
       );
 
+      setIsLoading(false);
       setRestaurant(response.data);
 
       const response2 = await axios.get(
@@ -39,83 +40,75 @@ const RestaurantMenu = () => {
 
   return (
     <div className="px-4 md:px-14">
-      {restaurant && restaurantOwner && isLoading ? (
-        <Loader />
-      ) : (
-        <>
-          {" "}
-          <div>
-            <div className="flex flex-col md:flex-row items-start justify-center md:justify-between border-b-2 border-gray-500 pb-14">
-              <div className="md:w-1/2 mb-4 md:mb-0">
-                <img
-                  src={restaurant.imageUrl}
-                  alt=""
-                  className="w-[80%] rounded-md shadow-md"
-                />
-              </div>
-              <div className="md:w-1/2 md:pl-4">
-                <div className="mb-4">
-                  <h2 className="text-2xl font-semibold mb-2">
-                    Restaurant Details
-                  </h2>
-                  <p className="text-gray-600">
-                    We provide these cuisines: {restaurant.cuisines.join(", ")}
-                  </p>
-                </div>
-                <div className="flex items-center mb-2 text-gray-600">
-                  <FiMapPin className="mr-1" />
-                  <p>
-                    {restaurant.city}, {restaurant.state}
-                  </p>
-                </div>
-                <div className="flex items-center mb-2 text-gray-600">
-                  <FiDollarSign className="mr-1" />
-                  <p>Delivery Price: ₹{restaurant.deliveryPrice}</p>
-                </div>
-                <div className="flex items-center mb-2 text-gray-600">
-                  <FiClock className="mr-1" />
-                  <p>
-                    Processing Time: {restaurant.estimatedDeliveryTime} minutes
-                  </p>
-                </div>
-                <div>
-                  <h2 className="text-2xl font-semibold mb-2 mt-6">
-                    Contact Us
-                  </h2>
-                  <p className="text-gray-600 mb-1">
-                    For any inquiries or feedback, feel free to reach out to us
-                    at:
-                  </p>
-                  <p className="text-gray-600 mb-1">
-                    Username: {restaurantOwner.username}
-                  </p>
-                  <p className="text-gray-600 mb-1">
-                    Email: {restaurantOwner.email}
-                  </p>
-                </div>
-              </div>
+      {isLoading ? <Loader /> : ""}
+      {restaurant && restaurantOwner && (
+        <div>
+          <div className="flex flex-col md:flex-row items-start justify-center md:justify-between border-b-2 border-gray-500 pb-14">
+            <div className="md:w-1/2 mb-4 md:mb-0">
+              <img
+                src={restaurant.imageUrl}
+                alt=""
+                className="w-[80%] rounded-md shadow-md"
+              />
             </div>
-
-            <div className="mt-8 border-b-2 border-gray-500 pb-14">
-              <h2 className="text-2xl font-semibold mb-4">
-                Explore our Dishes
-              </h2>
-              <div className="grid grid-cols-1 md:grid-cols-4 gap-4">
-                {restaurant.menuItems.map((menuItem, index) => (
-                  <MenuCart
-                    key={index}
-                    menuItem={menuItem}
-                    restaurantId={restaurant._id}
-                  />
-                ))}
+            <div className="md:w-1/2 md:pl-4">
+              <div className="mb-4">
+                <h2 className="text-2xl font-semibold mb-2">
+                  Restaurant Details
+                </h2>
+                <p className="text-gray-600">
+                  We provide these cuisines: {restaurant.cuisines.join(", ")}
+                </p>
               </div>
-            </div>
-
-            <div className="mt-8">
-              <Review />
+              <div className="flex items-center mb-2 text-gray-600">
+                <FiMapPin className="mr-1" />
+                <p>
+                  {restaurant.city}, {restaurant.state}
+                </p>
+              </div>
+              <div className="flex items-center mb-2 text-gray-600">
+                <FiDollarSign className="mr-1" />
+                <p>Delivery Price: ₹{restaurant.deliveryPrice}</p>
+              </div>
+              <div className="flex items-center mb-2 text-gray-600">
+                <FiClock className="mr-1" />
+                <p>
+                  Processing Time: {restaurant.estimatedDeliveryTime} minutes
+                </p>
+              </div>
+              <div>
+                <h2 className="text-2xl font-semibold mb-2 mt-6">Contact Us</h2>
+                <p className="text-gray-600 mb-1">
+                  For any inquiries or feedback, feel free to reach out to us
+                  at:
+                </p>
+                <p className="text-gray-600 mb-1">
+                  Username: {restaurantOwner.username}
+                </p>
+                <p className="text-gray-600 mb-1">
+                  Email: {restaurantOwner.email}
+                </p>
+              </div>
             </div>
           </div>
-        </>
+
+          <div className="mt-8 border-b-2 border-gray-500 pb-14">
+            <h2 className="text-2xl font-semibold mb-4">Explore our Dishes</h2>
+            <div className="grid grid-cols-1 md:grid-cols-4 gap-4">
+              {restaurant.menuItems.map((menuItem, index) => (
+                <MenuCart
+                  key={index}
+                  menuItem={menuItem}
+                  restaurantId={restaurant._id}
+                />
+              ))}
+            </div>
+          </div>
+
+          <div className="mt-8">
+            <Review />
+          </div>
+        </div>
       )}
     </div>
   );
